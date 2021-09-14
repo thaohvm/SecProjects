b0942b91a74228bb1151cebdbf99958a13710a0e
Thao Huynh <thaohvm@gmail.com>
2021-09-14 15:35:47 -0700

    update data for TODOapp in local storage

--- U4-04-TODOapp/TODOapp.js
+++ U4-04-TODOapp/TODOapp.js
@@ -2,6 +2,22 @@ const form = document.querySelector('#taskform');
 const taskInput = document.querySelector('input[name=newtask');
 const taskList = document.querySelector('#task-list');
 
+
+
+function updateData(data) {
+    let data = [];
+    let todos = document.getElementsByTagName('li');
+    for (let i = 0; i < todos.length; i++) {
+        let btnIndex = todos[i].innerText.lastIndexOf('Remove');
+        let todo = {
+            "todoText": todos[i].innerText.slice(0, btnIndex),
+            "todoStyle": todos[i].style.textDecoration
+        }
+        data.push(todo);
+    }
+    localStorage.setItem('data', JSON.stringify(data));
+}
+
 taskList.addEventListener('click', function(e) {
     if (e.target.tagName === "BUTTON") {
         e.target.parentElement.remove();
@@ -23,5 +39,5 @@ form.addEventListener('submit', function(e) {
     taskInput.value = '';
 })
 
-localStorage.setItem("task-list", JSON.stringify(taskList));
-JSON.parse(localStorage.getItem("task-list"))
+// localStorage.setItem("task-list", JSON.stringify(taskList));
+// JSON.parse(localStorage.getItem("task-list"))
