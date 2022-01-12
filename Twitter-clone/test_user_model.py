6f3a8f0fa20bc4d51743a07c68fc5af68c9c9e77
Thao Huynh <thaohvm@gmail.com>
2022-01-11 21:47:13 -0800

    first test

--- Twitter-clone/test_user_model.py
+++ Twitter-clone/test_user_model.py
@@ -7,6 +7,7 @@
 
 import os
 from unittest import TestCase
+from sqlalchemy import exc
 
 from models import db, User, Message, Follows
 
@@ -15,7 +16,7 @@ from models import db, User, Message, Follows
 # before we import our app, since that will have already
 # connected to the database
 
-os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+os.environ['DATABASE_URL'] = "postgresql:///warbler_test"
 
 
 # Now we can import app
@@ -28,6 +29,18 @@ from app import app
 
 db.create_all()
 
+def create_user():
+    u = User(
+            email="test@test.com",
+            username="testuser",
+            password="HASHED_PASSWORD"
+        )
+    u.id = 3333
+
+    db.session.add(u)
+    db.session.commit()
+
+    return u
 
 class UserModelTestCase(TestCase):
     """Test views for messages."""
@@ -35,9 +48,27 @@ class UserModelTestCase(TestCase):
     def setUp(self):
         """Create test client, add sample data."""
 
-        User.query.delete()
-        Message.query.delete()
-        Follows.query.delete()
+        db.drop_all()
+        db.create_all()
+
+        u1 = User.signup("test1", "email1@email.com", "password", None)
+        uid1 = 1111
+        u1.id = uid1
+
+        u2 = User.signup("test2", "email2@email.com", "password", None)
+        uid2 = 2222
+        u2.id = uid2
+
+        db.session.commit()
+
+        u1 = User.query.get(uid1)
+        u2 = User.query.get(uid2)
+
+        self.u1 = u1
+        self.uid1 = uid1
+
+        self.u2 = u2
+        self.uid2 = uid2
 
         self.client = app.test_client()
 
@@ -56,3 +87,13 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+
+    def tearDown(self):
+        res = super().tearDown()
+        db.session.rollback()
+        return res
+
+    def test_repr(self):
+        """Does repr work?"""
+
+        self.assertEqual(repr(self.u1), '<User #1111: testuser1, email1@test.com>')
