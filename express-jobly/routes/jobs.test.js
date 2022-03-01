147cd7d68a96f85e606beff8b2f5f8c9387d3c75
Thao Huynh <thaohvm@gmail.com>
2022-03-01 15:51:35 -0800

    update jobs routes

---
+++ express-jobly/routes/jobs.test.js
@@ -0,0 +1,146 @@
+"use strict";
+
+const request = require("supertest");
+
+const db = require("../db");
+const app = require("../app");
+
+const {
+    commonBeforeAll,
+    commonBeforeEach,
+    commonAfterEach,
+    commonAfterAll,
+    testJobIds,
+    u1Token,
+    adminToken,
+} = require("./_testCommon");
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
+/************************************** POST /jobs */
+
+describe("POST /jobs", function () {
+    const newJob = {
+        companyHandle: "c1",
+        title: "New",
+        salary: 10,
+        equity: "0.2"
+    };
+    test("admin can post job", async function () {
+        const resp = await request(app)
+            .post(`/jobs`)
+            .send(newJob)
+            .set("authorization", `Bearer ${adminToken}`);
+        expect(resp.statusCode).toEqual(201);
+        expect(resp.body).toEqual({
+            job: {
+                id: expect.any(Number),
+                companyHandle: "c1",
+                title: "New",
+                salary: 10,
+                equity: "0.2"
+            },
+        });
+    });
+    test("unauth for users cannot post job", async function () {
+        const resp = await request(app)
+            .post(`/jobs`)
+            .send(newJob)
+            .set("authorization", `Bearer ${u1Token}`);
+        expect(resp.statusCode).toEqual(401);
+    })
+
+    test("bad request with missing data", async function () {
+        const resp = await request(app)
+            .post(`/jobs`)
+            .send({
+                companyHandle: "c1"
+            })
+            .set("authorization", `Bearer ${adminToken}`);
+        expect(resp.statusCode).toEqual(400);
+    });
+    test("bad request with invalid data", async function () {
+        const resp = await request(app)
+            .post(`/jobs`)
+            .send({
+                companyHandle: "c1",
+                title: "J-new",
+                salary: "not a salary",
+                equity: "0.2",
+            })
+            .set("authorization", `Bearer ${adminToken}`);
+        expect(resp.statusCode).toEqual(400);
+    })
+})
+
+/************************************** GET /jobs */
+
+describe("GET /jobs", function () {
+    test("ok for anon", async function () {
+        const resp = await request(app).get(`/jobs`);
+        expect(resp.body).toEqual({
+            jobs: [
+                {
+                    id: expect.any(Number),
+                    title: "J1",
+                    salary: 1,
+                    equity: "0.1",
+                    companyHandle: "c1",
+                    companyName: "C1",
+                },
+                {
+                    id: expect.any(Number),
+                    title: "J2",
+                    salary: 2,
+                    equity: "0.2",
+                    companyHandle: "c1",
+                    companyName: "C1",
+                },
+                {
+                    id: expect.any(Number),
+                    title: "J3",
+                    salary: 3,
+                    equity: null,
+                    companyHandle: "c1",
+                    companyName: "C1",
+                },
+            ],
+        },
+        );
+    });
+    test("get job with filter", async function () {
+        const resp = await request(app)
+            .get(`/jobs`)
+            .query({ hasEquity: true });
+        expect(resp.body).toEqual({
+            jobs: [
+                {
+                    id: expect.any(Number),
+                    title: "J1",
+                    salary: 1,
+                    equity: "0.1",
+                    companyHandle: "c1",
+                    companyName: "C1",
+                },
+                {
+                    id: expect.any(Number),
+                    title: "J2",
+                    salary: 2,
+                    equity: "0.2",
+                    companyHandle: "c1",
+                    companyName: "C1",
+                },
+            ],
+        },
+        );
+    });
+    test("bad request on invalid filter key", async function () {
+        const resp = await request(app)
+            .get(`/jobs`)
+            .query({ minSalary: 2, nope: "nope" });
+        expect(resp.statusCode).toEqual(400);
+      });
+});
